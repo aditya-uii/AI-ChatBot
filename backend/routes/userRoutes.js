96ec38e3b9c14c659e0a3ddcd9d192762b198a5d
Aditya Kashyap <adityakashyap7998@gmail.com>
2025-07-29 23:18:24 +0530

    Log out routes added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signUp,login } from '../controllers/authController.js';
+import { signUp,login,logOut } from '../controllers/authController.js';
 
 const router = express.Router();
 
 
 router.post('/signup', signUp);
 router.post('/login', login);
+router.post('/logging-out', logOut);
 
 
 export default router;
