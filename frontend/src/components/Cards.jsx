9c0dd69c7434876441d90610bc5fc2a7f31819e7
Aditya Kashyap <adityakashyap7998@gmail.com>
2025-08-13 21:41:00 +0530

    n

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { userContextData } from '../context/UserContext';
+
 
 function Card({images}){
+
+
+    const { handleCurrentUserData,
+      frontendImage,setFrontendImage,
+      backendImage,setBackendImage,
+      selectedImage,setSelectedImage} = useContext(userContextData);
+
 return(
-    <div className=' w-[80px] h-[150px] lg:w-[150px] lg:h-[250px] bg-[#030326] border-4  border-[#0000ff6c] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-700 cursor-pointer hover:border-white'>
-<img src={images} alt="" className='h-full object-cover'/>
+    <div className={`w-[80px] h-[150px] lg:w-[150px] lg:h-[250px] bg-[#030326] border-4  border-[#0000ff6c] rounded-2xl overflow-hidden hover:shadow-2xl hover:shadow-blue-700 cursor-pointer hover:border-white
+    ${selectedImage==images? 'border-4 border-white shadow-2xl shadow-blue-700':null}
+    `}>
+<img src={images} alt="" className='h-full object-cover' onClick={()=>{setSelectedImage(images)}}/>
     </div>
 )
 }
