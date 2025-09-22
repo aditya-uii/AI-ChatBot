8ae77d04534e93cbf0b057e37749cdc0091df380
Aditya kashyap <132131143+aditya-uii@users.noreply.github.com>
2025-09-23 00:35:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ✅ CORS setup
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://ai-chatbot-frontend-yjj8.onrender.com",
   credentials: true 
 }));
 
